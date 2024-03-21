e77ffd28e6c749bd7a38710be89fbc7c1f2e6626
gbabohernest <gbaboh.ernest@gmail.com>
2024-03-21 21:17:12 +0000

    A script that creates a table users, with id, email, name attributes. Email attribute unique

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- A SQL script that creates a users table.
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id    INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name  VARCHAR(255)
+);
