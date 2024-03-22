9222cb7793ce7f42fbd9b848822eccb1dbe64b5f
Ernest P. Gbaboh <gbaboh.ernest@gmail.com>
2024-03-22 05:09:59 +0300

    Update 9-index_name_score.sql

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,4 @@
 -- A SQL script that creates an index idx_name_first
 -- on the table names and the first letter of name.
 
-CREATE INDEX idx_name_first_score ON names ((SUBSTRING(name, 1, 1)), score);
+CREATE INDEX idx_name_first_score ON names ( name(1), score );
