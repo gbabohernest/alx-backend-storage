8680b81d19f88a5c0ca0301e8c783fc81358dcc8
gbabohernest <gbaboh.ernest@gmail.com>
2024-03-22 01:40:30 +0000

    Average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,20 +5,12 @@
 DELIMITER //
 
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
-)
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(10, 2);
-
-    -- Compute the average score for the specified user
-    SELECT AVG(score) INTO avg_score FROM corrections
-    WHERE user_id = user_id;
-
-    -- Update the average score for the user in the users table
-    UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
+UPDATE users
+SET
+average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+WHERE id = user_id;
 END //
 
 DELIMITER ;
