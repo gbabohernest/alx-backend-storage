b55ec3881dd35fcb6d411d2c78bd11bcf3fb212a
Ernest P. Gbaboh <gbaboh.ernest@gmail.com>
2024-03-27 02:49:27 +0300

    Update 11-schools_by_topic.py

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -17,5 +17,5 @@ def schools_by_topic(mongo_collection, topic):
     :return:  list of school having a specific topic
     """
 
-    school_list = list(mongo_collection.find({"topic": topic}))
+    school_list = mongo_collection.find({"topic": topic})
     return school_list
