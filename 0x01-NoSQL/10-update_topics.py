b2ce28fe8f9db95f6c3c4e42ff0f695e457fed20
gbabohernest <gbaboh.ernest@gmail.com>
2024-03-26 16:22:23 -0700

    A function that changes all topics of a school document based on the name

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Write a Python function that changes all topics of a
+   school document based on the name:
+
+    Prototype: def update_topics(mongo_collection, name, topics):
+    mongo_collection will be the pymongo collection object
+    name (string) will be the school name to update
+    topics (list of strings) will be the list of topics approached
+    in the school
+"""
+
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based
+    on the name
+
+    :param mongo_collection:
+    :param name: school name to update
+    :param topics: List of topics
+    :return: updated school document
+    """
+
+    updated_doc = mongo_collection.update_one({"name": name},
+                                              {"$set": {"topics": topics}})
+    return updated_doc
